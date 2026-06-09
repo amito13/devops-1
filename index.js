eea792d6919a9204f18f76d5c2a0bdbd745a6e78
AmitDewangan <dewanganamit40@gmail.com>
2026-06-09 19:51:27 +0530

    port change

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.get('/', (req, res) => {
      res.send('Hello DevOps!');
 });
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+app.listen(8000, () => {
+    console.log('Server is running on port 8000');
 });
 
